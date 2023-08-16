42cb017adbdc0e193c327bdec52046508b8a0a9a
DelisG <delisemarciano@gmail.com>
2023-08-16 16:07:16 -0300

    🔖 Definindo o modelo de esquema

--- src/models/lists.js
+++ src/models/lists.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 
 const listSchema = new mongoose.Schema({
-  description: { type: String, required: true }, // Alteração feita aqui
-  date: { type: String, required: false },
-  hours: { type: String, required: false },
+  descricao: { type: String, required: true }, // Alteração feita aqui
+  data: { type: String, required: false },
+  hora: { type: String, required: false },
 });
 
 const List = mongoose.model("List", listSchema); // Renomeado para "List" (singular)
